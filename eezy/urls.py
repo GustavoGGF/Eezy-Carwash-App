e5d5d29172f18190d65034f3201915ca1bfa203d
GustavoGGF <gustavo@localhost.localdomain>
2023-03-07 15:28:49 -0300

    add file

---
+++ eezy/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+# Declarando as paginas do site
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('clientes/', include('client.urls')),
+    path('servicos/', include('services.urls')),
+]

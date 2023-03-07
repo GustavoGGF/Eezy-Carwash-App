1ec57c032543e3935e1df1920419719ff5bf1caa
GustavoGGF <gustavo@localhost.localdomain>
2023-03-07 16:16:43 -0300

    finaly day

--- services/forms.py
+++ services/forms.py
@@ -13,4 +13,8 @@ class FormService(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['titulo'].widget.attrs.update({'class': 'teste'})
+        for field in self.fields:
+            self.fields[field].widget.attrs.update(
+                {'class': 'form-control'})
+            self.fields[field].widget.attrs.update(
+                {'placeholder': field})

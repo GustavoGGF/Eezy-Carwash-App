319fb548c90890726429f4a0fd9440a95bd6911f
GustavoGGF <gustavo@localhost.localdomain>
2023-03-15 15:49:38 -0300

    finally services

--- services/views.py
+++ services/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 # trazendo o formulario pro HTML
 from .forms import FormService
+from django.http import HttpResponse
 
 # Tela de novo serviço
 
 
 def new_service(request):
-    form = FormService
-    # Colocando o form torna disponivel pelar ele no .html
-    return render(request, 'novo_servico.html', {'form': form})
+
+    if request.method == "GET":
+        form = FormService()
+        # Colocando o form torna disponivel pelar ele no .html
+        return render(request, 'novo_servico.html', {'form': form})
+    elif request.method == "POST":
+        form = FormService(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return  HttpResponse("Salvo com sucesso")
+        else:
+            return render(request, 'novo_servico.html', {'form': form})

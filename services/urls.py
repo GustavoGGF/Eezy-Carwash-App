fc81ea1b9f36cbfc58085cb18b69c072cef1369e
GustavoGGF <gustavo@localhost.localdomain>
2023-03-16 10:34:13 -0300

    update code

--- services/urls.py
+++ services/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('novo_servico/', views.new_service, name='novo_servico'),
     path('listar_servico/', views.list_service, name='listar_servico'),
-    path('servico/<str:identificador>', views.service, name='servico')
+    path('servico/<str:identificador>', views.service, name='servico'),
+    path('gerar_os/<str:identificador>', views.gerar_os, name='gerar_os'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 # trazendo o formulario pro HTML
 from .forms import FormService
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from .models import Service
+from fpdf import FPDF
+from io import BytesIO
 
 # Tela de novo serviço
 
@@ -15,19 +17,57 @@ def new_service(request):
         return render(request, 'novo_servico.html', {'form': form})
     elif request.method == "POST":
         form = FormService(request.POST)
-
+        
+        # Validando formularioo
         if form.is_valid():
             form.save()
+            # TODO fazer uma tela de retorno direito
             return  HttpResponse("Salvo com sucesso")
         else:
             return render(request, 'novo_servico.html', {'form': form})
 
+# Tela da lista de serviço
 def list_service(request):
     if request.method == "GET":
         services = Service.objects.all()
         return render(request, 'listar_servico.html', {'services': services})
     
+# Tela dos serviços
 def service(request, identificador):
     service = get_object_or_404(Service, identificador=identificador)
     
     return render(request, 'servico.html', {'servico': service})
+
+# Tela da geração de ordem de serviço
+def gerar_os(request, identificador):
+    servico = get_object_or_404(Service, identificador=identificador)
+    
+    # TODO estilizar melhor esse pdf
+    pdf = FPDF()
+    pdf.add_page()
+
+    pdf.set_font('Arial', 'B', 12)
+
+    pdf.set_fill_color(240, 240, 240)
+    pdf.cell(35, 10, 'Cliente', 1, 0,'L',1)
+    pdf.cell(0, 10, f'{servico.cliente.nome}', 1, 1,'L',1)
+
+    pdf.cell(35,10, 'Manutenções', 1, 0, 'L',1)
+
+    categorias_manutencao =  servico.maintenance_category.all()
+    for i, manutencao in enumerate(categorias_manutencao):
+        pdf.cell(0, 10, f'- {manutencao.get_titulo_display()}', 1, 1, 'L', 1)
+        if not i == len(categorias_manutencao) -1:
+            pdf.cell(35, 10, '', 0, 0)
+
+    pdf.cell(35,10,'Data de início',1,0,'L',1)
+    pdf.cell(0,10,f'{servico.data_inicio}',1,1,'L',1)
+    pdf.cell(35,10,f'Data de entrega',1,0,'L',1)
+    pdf.cell(0,10,f'{servico.data_entrega}',1,1,'L',1)
+    pdf.cell(35,10,f'Protocolo',1,0,'L',1)
+    pdf.cell(0,10,f'{servico.protocol}',1,1,'L',1)
+    
+    pdf_content = pdf.output(dest='S').encode('latin1')
+    pdf_bytes = BytesIO(pdf_content)
+
+    return FileResponse(pdf_bytes, as_attachment=True, filename="os.pdf")
